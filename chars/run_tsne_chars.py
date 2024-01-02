31029acb704c4a79bfdbb0dade9b16e9e4dcf836
Basile Terver <bisilou@gmail.com>
2024-01-02 17:49:58 +0100

    Added prints, changed params to reduce runtime

--- chars/run_tsne_chars.py
+++ chars/run_tsne_chars.py
@@ -10,13 +10,17 @@ from sta.utils import cost_matrix, tonumpy
 # change this if you have GPUs
 # in our platform, this experiment ran on 4 GPUs in around 8 minutes
 
-n_gpu_devices = 1
+n_gpu_devices = 0
 
 if __name__ == "__main__":
 
     t = time.time()
-    X = np.load("data/chars-processed.npy")
-    y = np.load("data/chars-labels-processed.npy")
+    # 20 samples per letter
+    X = np.load("data/chars-processed.npy")[:40]
+    print('X.shape=',X.shape)
+    y = np.load("data/chars-labels-processed.npy")[:40]
+    print('y.shape=',y.shape)
+    print('y=',y)
     n_samples, n_times, dimension, _ = X.shape
     epsilon = 1 / dimension
     gamma = 1.
@@ -27,14 +31,17 @@ if __name__ == "__main__":
     _, M = cost_matrix(dimension)
     K = tonumpy(torch.exp(- M / epsilon))
 
-    betas = [0, 0.001, 0.01, 0.1, 0.5, 1., 2., 3., 5., 10.]
+    # betas = [0, 0.001, 0.01, 0.1, 0.5, 1., 2., 3., 5., 10.]
+    betas=[0.1]
     experiment = dict(X=X, y=y, betas=betas, epsilon=epsilon, gamma=gamma)
     train_data = []
-    params = dict(K=K, epsilon=epsilon, gamma=gamma, n_jobs=4,
+    params = dict(K=K, epsilon=epsilon, gamma=gamma, n_jobs=1,
                   n_gpu_devices=n_gpu_devices)
 
     # compute sta distance matrix
     precomputed = sta_matrix(X, betas, **params)
+    print('-----sta_matrix precomputed -------')
+    
     experiment["sta"] = dict()
     for beta, train_ in zip(betas, precomputed):
         train = train_.copy()
@@ -47,10 +54,12 @@ if __name__ == "__main__":
     method = "soft"
     experiment["soft"] = dict()
     for beta in betas:
-        precomputed = sdtw_matrix(X, beta, n_jobs=10)
+        precomputed = sdtw_matrix(X, beta, n_jobs=2)
         train = precomputed.copy()
         # shift the distance to avoid negative values
         train -= train.min()
+        tsne = TSNE(metric="precomputed")
+        print("Default perplexity:", tsne.perplexity)
         tsne_data = TSNE(metric="precomputed").fit_transform(train)
         experiment[method][beta] = tsne_data
 
