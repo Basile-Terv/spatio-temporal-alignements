42d7e286b77781a7f35761542a71a172f750c2a0
Basile Terver <bisilou@gmail.com>
2024-01-03 00:10:42 +0100

    Added wandb functionality

--- chars/run_tsne_chars.py
+++ chars/run_tsne_chars.py
@@ -5,19 +5,23 @@ import time
 import torch
 from sta import sta_matrix, sdtw_matrix
 from sta.utils import cost_matrix, tonumpy
+# Added by me
+import wandb
+# from pathlib import Path
+
+# Initialize WandB
+wandb.init(project="spatio-temporal-alignments", name="chars_experiment")
 
 
 # change this if you have GPUs
 # in our platform, this experiment ran on 4 GPUs in around 8 minutes
-
 n_gpu_devices = 0
-perplexity=10 # Must be lower than nb of time series/samples, default value is 30
 
 if __name__ == "__main__":
 
     t = time.time()
     # 20 samples per letter
-    samples_per_letter = 10
+    samples_per_letter = 2
     total_letters = 7  # Assuming there are 7 letters
     X = np.concatenate([np.load(f"data/chars-processed.npy")[i * 20:(i * 20) + samples_per_letter] for i in range(total_letters)])
     print('X.shape=', X.shape)
@@ -29,6 +33,7 @@ if __name__ == "__main__":
     n_samples, n_times, dimension, _ = X.shape
     epsilon = 1 / dimension
     gamma = 1.
+    perplexity=10 # Must be lower than n_samples, default value is 30
 
     # create ground metrics. M corresponds to the convolutional distance
     # matrix for convolutional sinkhorn
@@ -42,6 +47,19 @@ if __name__ == "__main__":
     train_data = []
     params = dict(K=K, epsilon=epsilon, gamma=gamma, n_jobs=1,
                   n_gpu_devices=n_gpu_devices)
+
+    # Log configuration parameters to WandB
+    config = dict(
+        samples_per_letter=samples_per_letter,
+        total_letters=total_letters,
+        betas=betas,
+        epsilon=epsilon,
+        gamma=gamma,
+        perplexity=perplexity,
+        n_gpu_devices=n_gpu_devices,
+        # Add other relevant parameters
+    )
+    wandb.config.update(config)
 
     # compute sta distance matrix
     print('-----Starting sta_matrix computation-------')
@@ -68,7 +86,12 @@ if __name__ == "__main__":
         experiment[method][beta] = tsne_data
 
     # save all
-    expe_file = open("data/tsne-chars.pkl", "wb")
-    pickle.dump(experiment, expe_file)
+    expe_file_path = "data/tsne-chars.pkl"
+    with open(expe_file_path, "wb") as expe_file:
+        pickle.dump(experiment, expe_file)
+    
     t = time.time() - t
     print("Full time: ", t)
+
+    # Log file to WandB
+    wandb.save(expe_file_path)
